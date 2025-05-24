0db22b149161692f5fff3c662059cb846d7a599e
Alejandroduoc <alej.rodriguezl@duocuc.cl>
2025-05-24 13:52:35 -0400

    agregar id _sucursal

--- dist/routes/ventas.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const ventas_controller_1 = require("../controllers/ventas.controller");
-const router = (0, express_1.Router)();
-router.post('/ventas', ventas_controller_1.insertSale);
-exports.default = router;
-//# sourceMappingURL=ventas.routes.js.map
